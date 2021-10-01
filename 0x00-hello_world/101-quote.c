e48b807a70486964de7511df21be117555aafa7c
Juansepo13 <3896@holbertonschool.com>
2021-10-01 08:50:51 -0700

    Unix is basicaly a simple OS

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+  printf("and that piece of art useful\" - Dora Korpar, 2015-10-19\n);
+return (1)
+
+  
