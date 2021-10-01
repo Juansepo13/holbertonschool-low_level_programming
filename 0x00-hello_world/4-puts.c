a00de1a07e5270ac31f52f137b23c5ce4e186f14
Juansepo13 <3896@holbertonschool.com>
2021-09-30 20:27:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #!/bin/bash
-   /* "Programming is like building a multilingual puzzle" */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{ puts("\* "Programming is like building a multilingual puzzle");
+return (0);
+}
