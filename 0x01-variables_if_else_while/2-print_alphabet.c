24c16de8d5cc6a43b8f53c920fee709197a647bd
Juansepo13 <3896@holbertonschool.com>
2021-10-02 16:43:23 -0700

    I sometimes suffer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print of the alphabet lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
