23c119299dd07f2d6ba46259aa5ea1855f39905a
Juansepo13 <3896@holbertonschool.com>
2021-10-02 09:24:29 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Function description
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf(n);
+
+	printf('\n');
+
+	return (0);
+}
