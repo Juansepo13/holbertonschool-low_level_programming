1264a43b5822803b872de7a96869fb56e602dacf
Juansepo13 <3896@holbertonschool.com>
2021-10-04 08:47:39 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+		putchar(r);
+
+	putchar('\n');
+
+	retirm (0);
+}
