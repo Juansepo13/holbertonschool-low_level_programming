57612af36aa47069c95ff4e4afcf82d15e8976fb
Juansepo13 <3896@holbertonschool.com>
2021-10-04 09:01:24 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char q;
+	int w;
+
+	for (w = 0; w < 10; w++)
+		putchar(w + '0');
+
+	for (q = 'a'; q <= 'f'; q++)
+		putchar(q)
+
+			putchar('\w');
+
+	reurn (0);
+}
