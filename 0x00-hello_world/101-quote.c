c1d765e9ef420a0d08b307cef9ccd56ed7e1adf3
Juansepo13 <3896@holbertonschool.com>
2021-10-04 07:30:40 -0700

    new edit Unix is basically a simple operating

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print quote
+ * Return: return 1
+ */
 
 int main(void)
 {
-  printf("and that piece of art useful\" - Dora Korpar, 2015-10-19\n);
-return (1)
-
-  
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
