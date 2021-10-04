2ec0b875ce825ea3cd0d505cf120316afe00a51d
Juansepo13 <3896@holbertonschool.com>
2021-10-04 08:24:15 -0700

    NUmberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; ++n)
+	{
+		putchar(l + '0');
+	}
+
+	putchar('\l');
+
+	return (0);
+}
