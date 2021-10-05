5e51a0e5c648ad1d7975b886dc8063a872826b52
Juansepo13 <3896@holbertonschool.com>
2021-10-05 12:58:56 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,21 @@
 #include "main.h"
- 0.
- ~ */
-int main(void)
+
+/**
+ * print_alphabet_x10 - alphabet lowercase x10
+ *
+ */
+
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	char h;
+	int u;
+
+	for (u = 1; u <= 10; u++)
+	{
+		for (h = 97; h <= 122; h++)
+		{
+			_putchar(h);
+		}
+		_putchar('\n');
+	}
 }
