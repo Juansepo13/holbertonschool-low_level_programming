5bdf58e5a1d33ea47d339916f58258764afe76a9
Juansepo13 <3896@holbertonschool.com>
2021-10-05 16:03:52 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if character is lowercase
+ * @c: Character to print
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (( c>= 'a' && c c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
