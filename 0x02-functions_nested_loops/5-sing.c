6fa3113b76d271e48444d30e3c16d25a4ea9a01d
Juansepo13 <3896@holbertonschool.com>
2021-10-06 20:18:28 -0700

    add 5.sing.c

--- 0x02-functions_nested_loops/5-sing.c
+++ 0x02-functions_nested_loops/5-sing.c
@@ -2,27 +2,29 @@
 
 /**
  * print_sign - print sign
- * @n: parameter n
  *
- * Return: 1 or 0 or -1
+ * @n: The character to print
+ *
+ * Return: using two
  */
-
 int print_sign(int n)
 {
-	if (n > 0)
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	else if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else
 	{
 		_putchar('0');
 		return (0);
 	}
-	else
-	{
-		_putchar('-');
-		return (-1);
 
-	}
 }
