25c0f619c46b1ab8c0579797740a5742f1951e07
Juansepo13 <3896@holbertonschool.com>
2021-10-09 15:58:50 -0700

    add new parameter file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ * _isdigit - check a number between 0 to 9
+ * @c: Evalua al caracter
+ * Retrun: 1 if c is a Number
+ * 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int result;
+	if (48 <= c && 57 >= c)
+	{
+		result = 1;
+	}
+	else
+		result = 0;
+	retrun (result);
+}
