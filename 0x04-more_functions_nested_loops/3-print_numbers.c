54b1c700668683af37889e1ca69e7a4dd2686a68
Juansepo13 <3896@holbertonschool.com>
2021-10-09 17:00:10 -0700

    add new files 3.main.c, 3-print_numbers.c, _putchar.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Print number of 0 to 9
+ * Return: Always 0-
+ */
+
+void print_numbers(void)
+{
+	int e;
+
+	for (e = 48 ; e <= 57; e++)
+
+		_putchar(e);
+		_putchar('\n');
+}
