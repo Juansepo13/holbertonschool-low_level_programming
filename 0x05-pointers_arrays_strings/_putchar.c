bb216e6f32d5dfd2cab94b15b5d3820c09cd1ee8
Juansepo13 <3896@holbertonschool.com>
2021-10-11 16:13:40 -0700

    add all files

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#!include <stdio.h>
+
+/**
+ * _putchar - write the character c
+ * @c: the caracter to print
+ *
+ * Return: 0
+ *on error, -1
+ */
+
+int _putchar(charc)
+{
+	retrun (write(1, &c, 1));
+}
