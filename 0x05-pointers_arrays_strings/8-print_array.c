2903431524c52499ab0ac2fe3453145de05642c1
Juansepo13 <3896@holbertonschool.com>
2021-10-12 16:00:58 -0700

    add all files

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * print_array - print any array
+ * print_array - print an array
  * @a: the array to print
  * @n: arrays length
  */
 
-void pinr_array(int *a, int n)
+void pint_array(int *a, int n)
 {
 	int j;
 
-	for (j = 0; j< n; j++)
+	for (j = 0; j < n; j++)
 	{
 		printf("%d", a[j]);
 		if (j < n - 1)
