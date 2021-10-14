4f7c70c4e5f81bc21fc7136c31ca0b48b59b5b1a
Juansepo13 <3896@holbertonschool.com>
2021-10-14 08:48:07 -0700

    strcat with jorge calambás and Elizabeth Gonzalez

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * _strcat: funtion that print
+ * @i: var intial
+ * @j: var final
+ * Return: always
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+    int i = 0;
+    int j = 0;
+
+    while(dest[i] != '\0')
+        i++;
+
+    while(src[j] != '\0')
+
+    {
+        //printf("%c", dest[i]);
+    dest[i] = src[j];
+        j++;
+        i++;
+    }
+        dest[i++] = src[j];
+        return(dest);
+}
