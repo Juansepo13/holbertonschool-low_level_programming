f123e159613201cdbfcfe856652a28f31a26e1d9
Juansepo13 <3896@holbertonschool.com>
2021-10-18 21:50:33 -0700

    add for and putchar to print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,18 +8,18 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int b = 0, c = 0;
 
-	while (b < 8)
+	int y, x;
+
+	for (y = 0; y < 8; y++)
 	{
-		_putchar(a[b][c]);
-		c++;
-		if (c == 8)
+
+		for (x = 0; x < 8; x++)
 		{
-			_putchar('\n');
-			c = 0;
-			b++;
+			_putchar (a[y][x]);
 		}
+		_putchar ('\n');
+
 	}
+
 }
-.
