5840017679dabbf9686e66a09c638934a424a2e9
Juansepo13 <3896@holbertonschool.com>
2021-10-19 20:26:45 -0700

    add new _putchar

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,27 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_chessboard - prints out the chessboard
- * @a: a is a two dimensional array
+ * print_chessboard - Print the chessboard
+ * @a: Char to print
  *
- * Return: returns void
-*/
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 void print_chessboard(char (*a)[8])
 {
+	int i, x;
 
-	int chessrow;
-	int chesscol;
-
-	for (chessrow = 0; chessrow < 8;)
+	for (i = 0; i <= 7; i++)
 	{
-		for (chesscol = 0; chesscol < 8; chesscol++)
-		{
-			_putchar(a[chessrow][chesscol]);
-		}
-		_putchar(10);
-		chessrow++;
+		for (x = 0; x <= 7; x++)
+
+			_putchar(a[i][x]);
+
+		_putchar('\n');
 	}
 }
