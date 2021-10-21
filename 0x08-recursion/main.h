0cfd18fdee261402354a1b79e6407d42c3b5b027
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-20 23:08:33 -0500

    add new parameters to main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* Write a function that prints a string, followed by a new line.*/
+#include <unistd.h>
+#include <studio.h>
+/*CALL THE FUNTION _putchar*/
+int _putchar(char c);
+/*WRITE A FUNTION THAT PRINT A STRING FOLLOWED BY A NEW LINE*/
 void _puts_recursion(char *s);
 
 #endif /* MAIN_H */
