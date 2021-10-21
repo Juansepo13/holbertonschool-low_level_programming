b811597d09621e4d4768f676681bb3be8131a55a
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-21 18:52:38 -0500

    add new parameter int _sqrt_recursion(int n)

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+ MAIN_H
 #define MAIN_H
 #include <unistd.h>
 #include <stdio.h>
@@ -10,4 +10,10 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 /*Write a function that returns the length of a string.*/
 int _strlen_recursion(char *s);
+/*Write a function that returns the factorial of a given number.*/
+int factorial(int n);
+/*Write a function that returns the value of X raised to the power of Y*/
+int _pow_recursion(int x, int y);
+/* Write a function that returns the natural square root of a number. */
+int _sqrt_recursion(int n);
 #endif /* MAIN_H */
