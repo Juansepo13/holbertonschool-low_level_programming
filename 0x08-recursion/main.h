1177ee6ff403991da2f00637b199bfce4d464963
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-20 23:24:16 -0500

    add new prototype void _print_rev_recursion(char *s);

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 #include <unistd.h>
 #include <stdio.h>
+/* Funtion _putchar*/
 int _putchar(char c);
+/*Write a function that prints a string, followed by a new line*/
 void _puts_recursion(char *s);
-
+/*Write a function that prints a string in reverse*/
+void _print_rev_recursion(char *s);
 #endif /* MAIN_H */
-
