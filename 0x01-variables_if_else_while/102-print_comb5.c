fd9c13cf7ee8cefe579d02f9e6db9035aaefd1ba
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-25 10:22:48 -0500

    edit code file line 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 /**
- * main - prints numbers from 0 - 99.
- *
- * Return: 0
+ *main-prints the combinations of two two-digit numbers
+ *Return:0 if there is no error
  */
-
 int main(void)
 {
-	int c1 = 0, c2 = 1, m = 1;
-
-	while (c1 < 100)
+	int primero;
+	int segundo;
+	for (primero = 0; primero < 100; primero++)
 	{
-		c2 = m;
-		while (c2 < 100)
+		for (segundo = 0; segundo < 100; segundo++)
 		{
-			putchar(c1 / 10 + '0');
-			putchar(c1 % 10 + '0');
-			putchar(' ');
-			putchar(c2 / 10 + '0');
-			putchar(c2 % 10 + '0');
-			if ((c1 == 99) && (c2 == 99))
+			if (primero < segundo)
 			{
-				break;
-			}
-			else
-			{
-				putchar(',');
+				putchar((primero / 10) + 48);
+				putchar((primero % 10) + 48);
 				putchar(' ');
+				putchar((segundo / 10) + 48);
+				putchar((segundo % 10) + 48);
+				if (primero != 98 || segundo != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			c2++;
 		}
-		c1++;
-		m++;
 	}
 	putchar('\n');
 	return (0);
