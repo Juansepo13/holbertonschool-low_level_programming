42dfa127aa2d432c4db7e2bc8598f67ca714f2e5
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-25 10:24:16 -0500

    add blank space in line 31

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,23 +3,31 @@
  *main-prints the combinations of two two-digit numbers
  *Return:0 if there is no error
  */
+
 int main(void)
 {
 	int primero;
+
 	int segundo;
+
 	for (primero = 0; primero < 100; primero++)
 	{
+
 		for (segundo = 0; segundo < 100; segundo++)
 		{
+
 			if (primero < segundo)
 			{
+
 				putchar((primero / 10) + 48);
 				putchar((primero % 10) + 48);
 				putchar(' ');
 				putchar((segundo / 10) + 48);
 				putchar((segundo % 10) + 48);
+
 				if (primero != 98 || segundo != 99)
 				{
+
 					putchar(',');
 					putchar(' ');
 				}
