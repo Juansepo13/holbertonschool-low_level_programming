568ab0636092a6184589f420b0641562d5611e4e
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-26 15:39:22 -0500

    add file 3-strspn.c add file 3-strspn.c repo 0x07

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - returns the number of the characters that
+ * are matched in the given string.
+ * @s: string to be scanned
+ * @accept: string that is scanned for
+ *
+ * Return: number of bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+		if (check == 0)
+			return (value);
+	}
+	return (value);
+}
