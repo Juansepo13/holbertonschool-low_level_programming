38ba624409d7c93fde0dd6fe648a7e91668f721b
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-26 16:23:04 -0500

    add file 9-strcpy.c repo 0x05

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - Funtion thar copy the string
+ * @dest: data
+ * @src: data
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int b = 0;
+
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[b] = src[b];
+	return (dest);
+}
