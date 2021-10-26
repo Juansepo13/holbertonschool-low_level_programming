240a19e87b7ddcc698b33bd14f7f6c3f1cea4283
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-26 17:46:42 -0500

    add file more first prototype char *create_array(unsigned int size, char c);

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Write a function that creates an array of chars, and initializes it with a specific char. */
+char *create_array(unsigned int size, char c);
+
+#endif
