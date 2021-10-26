97f6fa375afbf647ef13b45ae927bcfc93a511aa
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-26 13:31:18 -0500

    add 1-isdigit.c repo 0x04

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - check a number between 0 to 9
+ * @c: Evalua al caracter
+ * Return: 1 if c is a Number
+ * 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int result;
+
+	if (48 <= c && 57 >= c)
+	{
+		result = 1;
+	}
+
+	else
+		result = 0;
+
+	return (result);
+}
