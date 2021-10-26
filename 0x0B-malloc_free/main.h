93b903b1d862d71e81eaff7077ff77a52f5656d2
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-26 18:05:01 -0500

    add new prototype char *_strdup(char *str);

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 /* Write a function that creates an array of chars, and initializes it with a specific char. */
 char *create_array(unsigned int size, char c);
+/* Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter */
+char *_strdup(char *str);
 
 #endif
