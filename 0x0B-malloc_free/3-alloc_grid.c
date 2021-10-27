ebc0321401e67f3ef1eda1b0d9906c69761f55d8
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-27 15:36:34 -0500

    correct file, edit line 3 alloc-grid for alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,29 +2,42 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * alloc-grid - Allocate a grid
+ * alloc_grid - Pointer to 2 array
  * @width: width of array
  * @height: height of array
  *
  * Return: Pointer to array
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **array, i, j;
+	int i = 0;
+	int **alloc_grid;
 
-	if (width < 1 || height < 1)
-		return (NULL);
-	array = malloc(height * sizeof(int *));
-	if (array == 0)
+	if (width <= 0 || height <= 0)
 	{
-		free(array);
+		return (NULL);
+	}
+	alloc_grid = (int **)malloc(height * sizeof(int *));
+	if (alloc_grid == NULL)
+	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++)
-			array[i][j] = 0;
+		alloc_grid[i] = (int *)malloc(width * sizeof(int));
+		if (alloc_grid[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(alloc_grid[i]);
+			}
+			free(alloc_grid);
+			return (NULL);
+		}
 	}
-	return (array);
+	if (alloc_grid == NULL)
+	{
+		return (NULL);
+	}
+	return (alloc_grid);
 }
