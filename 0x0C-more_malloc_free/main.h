34f0d9f07934c2560ed6c6d1eb6eacfc13c60dda
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-28 19:20:02 -0500

    Prototype: void *malloc_checked(unsigned int b); Write a function that allocates memory using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 /* Write a function that allocates memory using malloc. */
 void *malloc_checked(unsigned int b);
+/* Write a function that allocates memory using malloc */
+Prototype: void *malloc_checked(unsigned int b);
 
 #endif
