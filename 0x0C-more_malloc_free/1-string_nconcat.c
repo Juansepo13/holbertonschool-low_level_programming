1eab1113a5c909fa33ab89fa3e4053bb75a81322
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-28 19:26:23 -0500

    Write a function that concatenates two strings.
    Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
    The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
    If the function fails, it should return NULL
    If n is greater or equal to the length of s2 then use the entire string s2
    if NULL is passed, treat it as an empty string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_nconcat - function that concatenates 2 strings
+ * @s1: string1
+ * @s2: string2
+ * @n: bytes to s2 copied
+ *
+ * Return: Pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+        char *fifi = NULL;
+	unsigned int i, c, d;
+
+	if (s1 == '\0')
+	{
+		s1 = "";
+	}
+	if (s2 == '\0')
+	{
+		s2 = "";
+	}
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+	}
+	for (d = 0; s2[d] != '\0'; d++)
+	{
+	}
+	if (n >= d)
+	{
+		n = d;
+	}
+	fifi = malloc((c + n + 1) * sizeof(char));
+
+	if (fifi == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		fifi[i] = s1[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		fifi[c + i] = s2[i];
+	}
+	fifi[c + n] = '\0';
+	return (fifi);
