f2b0ef0711fb0938947059ff43bf2a70938b0115
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-29 16:59:46 -0500

    add prototype int *array_range(int min, int max);

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 /* Write a function that allocates memory for an array, using malloc */
 void *_calloc(unsigned int nmemb, unsigned int size);
+/* Write a function that creates an array of integers. */
+int *array_range(int min, int max);
 
 #endif
