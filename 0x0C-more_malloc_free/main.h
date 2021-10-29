8a464062bad3964270f1f28148b7cf9143f3d306
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-29 16:23:02 -0500

    add prototype void *_calloc(unsigned int nmemb, unsigned int size);

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,7 @@
 void *malloc_checked(unsigned int b);
 /* Write a function that concatenates two strings. */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+/* Write a function that allocates memory for an array, using malloc */
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
