52b249377fb74a194f40544edfb302aec68793be
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-10-28 19:21:33 -0500

    changue prototype and description

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 /* Write a function that allocates memory using malloc. */
 void *malloc_checked(unsigned int b);
-/* Write a function that allocates memory using malloc */
-Prototype: void *malloc_checked(unsigned int b);
+/* Write a function that concatenates two strings. */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
