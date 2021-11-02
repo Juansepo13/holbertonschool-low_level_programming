d0fbed1772b831d0aa9b01e45baff4972edd4df7
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-11-01 19:47:31 -0500

    add Write a function that frees dogs.
    Prototype: void free_dog(dog_t *d);

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - Frees dogs.
+ * @d: Pointer.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
