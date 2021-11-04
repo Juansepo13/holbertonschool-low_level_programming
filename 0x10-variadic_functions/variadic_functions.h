069324304597f079258f9ef7d63d45bed396f70d
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-11-04 16:59:53 -0500

    add all prottotypes on variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,12 @@ typedef struct print
 int _putchar(char c);
 /* Write a function that returns the sum of all its parameters. */
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(va_list mylist);
+void print_i(va_list mylist);
+void print_f(va_list mylist);
+void print_s(va_list mylist);
 
 #endif
