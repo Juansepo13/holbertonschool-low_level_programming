d4d409fb8a76c297fbb0bd02800f6fef93ac5abb
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-11-04 09:38:59 -0500

    add spaces in declaration

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- *array_iterator - Executes function given as parameters on array's elements.
- *@array: Array + Elements.
- *@size: Array size.
- *@action: Pointer to the function.
+ * array_iterator - Executes function given as parameters on array's elements.
+ * @array: Array + Elements.
+ * @size: Array size.
+ * @action: Pointer to the function.
+ * Return: none.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
