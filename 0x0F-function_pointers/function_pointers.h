9eb9cb46eb7c66883394482419171d079cb62e27
Juanpo13 <root@DESKTOP-GKO2DPR.localdomain>
2021-11-04 09:40:01 -0500

    add new prototype Write a function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,7 @@
 int _putchar(char c);
 /* Write a function that prints a name */
 void print_name(char *name, void (*f)(char *));
+/* Write a function that executes a function given as a parameter on each element of an array. */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
