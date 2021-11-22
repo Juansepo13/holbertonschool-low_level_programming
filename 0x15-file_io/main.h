6c1180994959e12098943474f7287972e242c738
Juansepo13 <3895@holbertonschool.com>
2021-11-22 10:02:41 -0500

    add all files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,12 +4,13 @@
 #include <stdio.h>
 
 int _putchar(char c);
-/*  */
-
-/*  */
-
-/*  */
-
+/*Write a function that reads a text file and prints it to the POSIX standard*/
+/*output.*/
+ssize_t read_textfile(const char *filename, size_t letters);
+/*Create a function that creates a file. */
+int create_file(const char *filename, char *text_content);
+/*Write a function that appends text at the end of a file.*/
+int append_text_to_file(const char *filename, char *text_content);
 /*  */
 
 /*  */
