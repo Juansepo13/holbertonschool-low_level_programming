05b7ce739f742d044dd9176b64b084ccee9541f9
Juansepo13 <3895@holbertonschool.com>
2022-01-27 14:46:09 -0500

    add file  1.dbj2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,27 +10,3 @@ unsigned long int hash_djb2(const unsigned char *str)
 	}
 	return (hash);
 }
-julien@ubuntu:~/0x1A. Hash tables$
-julien@ubuntu:~/0x1A. Hash tables$ cat 1-main.c
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "hash_tables.h"
-
-		/**
-		 * main - check the code
-		 *
-		 * Return: Always EXIT_SUCCESS.
-		 */
-int main(void)
-{
-	char *s;
-
-	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "98";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	return (EXIT_SUCCESS);
-}
