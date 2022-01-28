37c0ed21536237a7eea68d21b8e0df52fe1d0bb7
Juansepo13 <3895@holbertonschool.com>
2022-01-28 18:28:40 -0500

    add all filesasd

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
+	unsigned long int i = 0;
 
-	if (size <= 0)
-		return (NULL);
 	hash_table = malloc(sizeof(hash_table_t) * 1);
 	if (hash_table == NULL)
+	{
 		return (NULL);
+	}
 	hash_table->size = size;
-	hash_table->array =  calloc(size, sizeof(hash_node_t *));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
-		free(hash_table);
 		return (NULL);
+	}
+	for (; i < size ; i++)
+	{
+		hash_table->array[i] = NULL;
 	}
 	return (hash_table);
 }
