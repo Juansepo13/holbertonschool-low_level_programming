8005009c4ec2183426a9d2035af9f32270166362
Juansepo13 <3895@holbertonschool.com>
2022-01-28 18:33:27 -0500

    add all files

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,8 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -40,5 +40,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-
-#endif /* HASH_TABLES_H */
+#endif
