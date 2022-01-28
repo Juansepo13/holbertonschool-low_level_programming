4a202244eb6ed605f195accc8731c5ea308e4a0b
Juansepo13 <3895@holbertonschool.com>
2022-01-28 18:26:15 -0500

    delete space in linew 15

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *NewHashTable;
+	hash_table_t *hash_table = NULL;
 
-	NewHashTable = malloc(sizeof(hash_table_t));
-	if (NewHashTable == NULL)
+	if (size <= 0)
 		return (NULL);
-
-	NewHashTable->array = malloc(sizeof(hash_node_t *) * size);
-	NewHashTable->size = size;
-
-	return (NewHashTable);
+	hash_table = malloc(sizeof(hash_table_t) * 1);
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array =  calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	return (hash_table);
 }
